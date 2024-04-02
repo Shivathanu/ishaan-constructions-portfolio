734523ee0c44c3528398ab94ef8c6a1854de607c
Shivathanu <gcshivathanu@gmail.com>
2024-04-02 19:05:40 +0530

    added mobile hero map

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -20,6 +20,21 @@ export default function Page() {
         imageHeight: 100,
         date: '2022-02-22',
         timeToRead: 5,
+        experience: {
+          details: [
+            'A Strategic professional with 30+ years of extensive experience in Project Planning & Management in the Construction sector.',
+            'Comprehensive experience in civil construction undertaking operations related to Cost Estimation & Budgeting, Design Reviewing, Procurement & Material Management, Client Interaction and MEP Trade Estimation.',
+            'Proven track record in leading multidisciplinary teams throughout all project phases, from conceptualization and planning to execution and completion. Skilled in project scheduling, budgeting, resource allocation, and risk management to ensure timely and cost-effective project delivery.',
+            'Extensive experience overseeing construction activities to ensure adherence to specifications, codes, and quality standards. Proficient in resolving on-site challenges, coordinating with contractors, and implementing effective construction methodologies to optimize project outcomes.',
+            'In-depth knowledge of regulatory requirements and permitting processes governing civil engineering projects. Demonstrated ability to navigate complex regulatory frameworks and obtain necessary approvals while ensuring compliance with environmental, safety, and zoning regulations.',
+            'Expertise in evolving systems / procedures in civil construction domain for change management in Project Operations entailing mapping business processes and studying workflow to designing / installation and commissioning of solutions, ensuring business functionality adherence as well as enhancing competitive advantage.',
+            'Proficiency in managing development and implementation of key procurement & sourcing / alternate sourcing strategies (raw material, process items & maintenance items) & contingency plans, ensuring that plans are aligned with project as well as the production / project requirements for various units.',
+            'Skilled in ensuring effective site management and discipline to ensure safety / hygiene & timely execution of projects in accordance with standards pertaining to machinery & equipment.',
+            'Strong relationship management & communication capabilities with skills to identify & network with project engineers, engineering consultants / clients / vendors / suppliers / sub-contractors / local officials / authority with consummate ease as well as obtaining necessary approvals for the projects.',
+            'Adept at identifying and resolving technical challenges, design constraints, and construction issues through innovative problem-solving approaches. Possess a keen attention to detail and a proactive mindset to address project complexities and optimize project outcomes.',
+            'With over three decades of hands-on experience and a proven track record of delivering successful civil engineering projects, I am well-equipped to provide strategic leadership, technical expertise, and innovative solutions to drive the success of future endeavors in the field'
+          ]
+        }
       }}>
       </AuthorLayout>
     </>
--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -1,3 +1,6 @@
+import SocialIcon from "@/components/social-icons"
+import Image from "next/image"
+
 interface Props {
   content: any
 }
@@ -5,42 +8,51 @@ interface Props {
 export default function AuthorLayout({ content }: Props) {
   return (
     <>
-      <div className="max-container padding-container bg-blue-900 h-screen py-20 divide-y divide-gray-200 dark:divide-gray-200">
+      <section className="max-container padding-container bg-blue-900 h-full py-20">
         <div className="space-y-2 pb-8 pt-20 md:space-y-5">
-          <span className="bold-40 md:bold-52 lg:bold-88 text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-600">About Us</span>
+          <span className="font-bold text-5xl text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-600">About Us</span>
+        </div>
+        <div className="prose max-w-none pb-14 pt-8 dark:prose-invert xl:col-span-2">
+          <p>Ishaan Infra Projects is a dynamic and dedicated small construction company specializing in delivering
+          personalized service and high-quality craftsmanship. With a focus on attention to detail and
+          customer satisfaction, we take pride in turning our clients&#39; visions into reality.
+          </p>
+
+          <p>At Ishaan Infra Projects, our mission is to provide exceptional construction services tailored to the
+          unique needs of each client. We strive to build lasting relationships based on trust, reliability, and
+          superior workmanship.
+          </p>
+        </div>
+        <hr className="py-8 border-1" />
+        <div className="space-y-2 pb-2 pt-2 md:space-y-5">
+          <span className="font-bold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-pink-400 to-orange-600">Our Founder&apos;s Story</span>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
-          {/* <div className="flex flex-col items-center space-x-2 pt-8">
-            {image && (
+          <div className="flex flex-col items-center space-x-2 pt-8">
+            {content.image && (
               <Image
-                src={image}
+                src={content.image}
                 alt="image"
                 width={192}
                 height={192}
-                className="h-48 w-48 rounded-full"
+                className="h-58 w-48 rounded-full"
               />
             )}
-            <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{content.name}</h3>
+            <div className="text-gray-500 dark:text-gray-400">{content.occupation}</div>
+            <div className="text-gray-500 dark:text-gray-400">{content.company}</div>
             <div className="mb-3 mt-3 flex space-x-4">
-              <SocialIcon kind="mail" href={`mailto:${email}`} size={6} />
-              <SocialIcon kind="linkedin" href={linkedin} size={6} />
+              <SocialIcon kind="mail" href={`mailto:${content.email}`} size={6} />
+              <SocialIcon kind="linkedin" href={content.linkedin} size={6} />
             </div>
-          </div> */}
-          <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
-            <p>Ishaan Infra Projects is a dynamic and dedicated small construction company specializing in delivering
-            personalized service and high-quality craftsmanship. With a focus on attention to detail and
-            customer satisfaction, we take pride in turning our clients&#39; visions into reality.
-            </p>
-
-            <p>At Ishaan Infra Projects, our mission is to provide exceptional construction services tailored to the
-            unique needs of each client. We strive to build lasting relationships based on trust, reliability, and
-            superior workmanship.
-            </p>
+          </div>
+          <div className="prose max-w-none pb-14 pt-8 dark:prose-invert xl:col-span-2">
+            {content.experience.details.map((detail, index) => (
+              <p key={index} className="text-md mb-2">{detail}</p>
+            ))}
           </div>
         </div>
-      </div>
+      </section>
     </>
   )
 }
